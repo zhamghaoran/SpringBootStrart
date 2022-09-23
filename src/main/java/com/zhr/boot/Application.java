2adfb284d5a8acc6ceafd3b3935258b29912a6ba
zhanghaoran <2017929959@qq.com>
2022-09-23 11:30:16 +0800

    Initial commit

---
+++ src/main/java/com/zhr/boot/Application.java
@@ -0,0 +1,15 @@
+package com.zhr.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @SpringBootApplication 这是一个springboot应用
+ */
+@SpringBootApplication
+public class Mainapplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Mainapplication.class,args);
+    }
+
+}
---
+++ src/main/java/com/zhr/boot/Controller/HelloController.java
@@ -0,0 +1,4 @@
+package com.zhr.boot.Controller;
+
+public class Hello {
+}
