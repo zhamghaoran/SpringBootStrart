3f85e31e22bf5d06f90ba7ecec8e4d587f20bb38
zhanghaoran <2017929959@qq.com>
2022-09-23 11:30:33 +0800

    ""

--- src/main/java/com/zhr/boot/Application.java
+++ src/main/java/com/zhr/boot/Application.java
@@ -4,12 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * @SpringBootApplication 这是一个springboot应用
+ * {@code @SpringBootApplication} 这是一个springboot应用
  */
 @SpringBootApplication
-public class Mainapplication {
+public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Mainapplication.class,args);
+        SpringApplication.run(Application.class,args);
     }
 
 }
--- src/main/java/com/zhr/boot/Controller/HelloController.java
+++ src/main/java/com/zhr/boot/Controller/HelloController.java
@@ -1,4 +1,13 @@
 package com.zhr.boot.Controller;
 
-public class Hello {
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public String handle01() {
+        return "qaq";
+    }
+
 }
